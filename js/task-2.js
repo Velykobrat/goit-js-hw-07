1340a59bfd9258a747fc214b34363d15153f7f17
Vitalii <149200949+Velykobrat@users.noreply.github.com>
2024-03-13 18:39:51 +0200

    1 та 2 завдання зробив

--- js/task-2.js
+++ js/task-2.js
@@ -11,19 +11,25 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-  {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
 ];
+
+
+const galleryList = document.querySelector('.gallery');
+
+// Створення фрагмента для оптимізації додавання елементів
+const fragment = document.createDocumentFragment();
+
+images.forEach(image => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+
+    img.src = image.url;
+    img.alt = image.alt;
+    img.classList.add('gallery-image'); // Додати клас для стилізації
+
+    li.appendChild(img);
+    fragment.appendChild(li);
+});
+
+// Додавання фрагмента з усіма елементами до списку галереї
+galleryList.appendChild(fragment);
